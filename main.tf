d6ea722775305ff665fb645de9e46343ebd61e56
psakthiece <80499338+psakthiece@users.noreply.github.com>
2026-03-28 14:34:35 +0530

    Update main.tf
    Updated

--- main.tf
+++ main.tf
@@ -2,12 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Use a public key file stored in your Terraform project directory
+# Register the public key with AWS
 resource "aws_key_pair" "my_key" {
   key_name   = "my-key"
-  public_key = file("${path.module}/mykey.pub")   # place mykey.pub in same folder as main.tf
+  public_key = file("${path.module}/mykey.pub")   # this file is now present
 }
 
+# Launch EC2 instance
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0c3389a4fa5bddaad" # Amazon Linux 2 AMI (check region-specific AMI)
   instance_type = "t3.small"
@@ -18,7 +19,7 @@ resource "aws_instance" "my_ec2" {
   }
 }
 
-# Output the public IP so you can connect easily
+# Output the public IP
 output "ec2_public_ip" {
   value = aws_instance.my_ec2.public_ip
 }
