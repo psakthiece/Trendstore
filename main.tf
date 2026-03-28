a052543bc0ca65a711faa9eec8640e15be272b7b
psakthiece <80499338+psakthiece@users.noreply.github.com>
2026-03-28 13:31:12 +0530

    Update main.tf
    updated

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ resource "aws_key_pair" "my_key" {
 
 # Create EC2 instance
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c55b159cbfafe1f0" # Example Amazon Linux 2 AMI
-  instance_type = "t2.micro"
+  ami           = "ami-0c3389a4fa5bddaad" # Example Amazon Linux 2 AMI
+  instance_type = "t3.small"
   key_name      = aws_key_pair.my_key.key_name
 
   tags = {
-    Name = "MyTerraformEC2"
+    Name = "Jenkinsserver"
   }
 }
 
